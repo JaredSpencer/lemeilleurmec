21bcbc01048fcd08a8ca16ceeac92f0127b53749
JARED SPENCER <jareddespencer@gmail.com>
2017-07-15 16:42:39 -0700

    override directory with create-react-app for deploying to heroku.

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export function selectBook(book) {
-  // console.log('this is the book ' + book.title);
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  }
-}
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'Book1', pages: 111},
-    { title: 'Book2', pages: 222},
-    { title: 'Book3', pages: 333},
-    { title: 'Book4', pages: 444}
-  ]
-}
